dc614e09b65b86b492461db61d75f05343aefbb5
wonmyeong <109929448+wonmyeong@users.noreply.github.com>
2024-07-31 15:18:47 +0900

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+
+st.title("펫 GPT")
+st.markdown("닥터 아몬드 펫GPT에 오신 것을 환영합니다. 펫 GPT는 반려견 정보 전용 GPT로 반려견 건강 전문 GPT 프로그램 입니다")
---
+++ Pages/app.py
@@ -0,0 +1,119 @@
+import os
+import time
+from typing import Dict, List
+from uuid import UUID
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.embeddings import CacheBackedEmbeddings, OpenAIEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.callbacks import StreamingStdOutCallbackHandler
+import streamlit as st
+from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.base import BaseCallbackHandler
+
+st.set_page_config(
+    page_title="PetGPT",
+    page_icon="🐶",
+)
+
+class ChatCallbackHandler(BaseCallbackHandler):
+    message = ""
+    
+    def on_llm_start(self, *args, **kwargs):
+        self.message_box = st.empty()
+    
+    def on_llm_end(self, *args, **kwargs):
+        save_message(self.message, "ai")
+        
+    def on_llm_new_token(self, token, *args, **kwargs):
+        self.message += token
+        self.message_box.markdown(self.message)
+
+llm = ChatOpenAI(temperature=0.1, streaming=True, callbacks=[ChatCallbackHandler()])
+
+@st.cache_data(show_spinner="Embedding file...")
+def embed_file(file_path):
+    
+    file_name = os.path.basename(file_path)
+    cache_dir = LocalFileStore(f"./embeddings_cache/{file_name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings()
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+def save_message(message, role):
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = []
+    st.session_state["messages"].append({"message": message, "role": role})
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        save_message(message, role)
+
+def paint_history():
+    if "messages" in st.session_state:
+        for message in st.session_state["messages"]:
+            send_message(message["message"], message["role"], save=False)
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            Context : {context}
+            """
+        ),
+        ("human", "{question}")
+    ]
+)
+
+st.title("PetGPT")
+
+st.markdown(
+    """
+    안녕하세요 PetGPT입니다!!!!
+    반려견에 관해 궁금한 점 있으시면 질문해 주세요🐕🐕🐕
+    """
+)
+
+# Directly specify the file path you want to use
+# file_path = "C:\\Users\\wmk51\\Final_Project\\.cache\\files\\dog_health.txt"
+file_path = ".cache\\files\\dog_health.txt"
+
+retriever = embed_file(file_path)
+# send_message("I'm ready! Ask away!", "ai", save=False)
+paint_history()
+message = st.chat_input("메세지 PegGPT")
+
+if message:
+    send_message(message, "human")
+    chain = ({
+        "context": retriever | RunnableLambda(format_docs),
+        "question": RunnablePassthrough(),
+    }
+    | prompt
+    | llm)
+    
+    with st.chat_message("ai"):
+        response = chain.invoke(message)
+        
+
+
